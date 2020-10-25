135b97ae6ea8e609bf5dc77d77577b7d2e3bb738
mandareis <amanda@depaula.xyz>
2020-10-25 13:14:42 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Amanda"
+  end
+  get "/hometown" do
+    "My hometown is Rio de Janeiro"
+  end
+  get "/favorite-song" do
+    "My favorite song is Promise by Ben Howard"
+  end
 end
